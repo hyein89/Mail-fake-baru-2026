730716433387e67d9f2bd7b75b2707ddd3a4c54a
hyein89 <bihontai@gmail.com>
2026-03-11 23:45:48 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   // Nama website Anda (untuk Navbar dan Footer)
-  name: "TempMail",
+  name: "EuorgFMail",
   
   // Domain URL website Anda (bukan domain untuk alamat email)
   domain: "mail-fake-baru-2026.vercel.app"
